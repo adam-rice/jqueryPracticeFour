7cf2bf9d2b6f31a72e68fcbbbfad5a324a7ab221
Adam Rice <adamcoltonrice@gmail.com>
2016-10-09 00:21:19 -0600

    complete

--- scripts.js
+++ scripts.js
@@ -1,7 +1,32 @@
-// $('#slider .slides').animate({'margin-left': -720}, 1000);
 
 $(function () {
-  setInterval(function() {
-    $('#slider .slides').animate({'margin-left': '-=720px'}, 1000);
-  }, 3000);
+  var width = 720;
+  var animationSpeed = 1200;
+  var hold = 2500;
+  var currentSlide = 1;
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slides');
+  var $slides = $slideContainer.find('.slide');
+
+  var interval;
+
+  function resumeSlider() {
+    interval = setInterval(function() {
+      $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+        currentSlide++;
+        if (currentSlide === $slides.length) {
+          currentSlide = 1;
+          $slideContainer.css('margin-left', 0);
+        }
+      });
+    }, hold);
+  }
+
+  function pauseSlider() {
+    clearInterval(interval);
+  }
+
+  $slider.on('mouseenter', pauseSlider).on('mouseleave', resumeSlider);
+
+  resumeSlider();
 });
